6b0bbf15f62fed893b060f023d8c4f9ed494dd98
Luxalpa <git@luxalpa.io>
2024-06-13 12:49:26 +0200

    initial problem

--- examples/leptos-animate-test/src/app.rs
+++ examples/leptos-animate-test/src/app.rs
@@ -31,12 +31,45 @@ pub fn App() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
-    let next_val = StoredValue::new(6);
+    let next_key = StoredValue::new(6);
     let elements = RwSignal::new(vec![1, 2, 3, 4, 5]);
 
-    let on_click = move |_| {
-        elements.update(|v| v.push(next_val.get_value()));
-        next_val.update_value(|v| *v += 1);
+    let get_next_key = move || {
+        let v = next_key.get_value();
+        next_key.update_value(|v| *v += 1);
+        v
+    };
+
+    let add_one = move |_| {
+        elements.update(|v| v.push(get_next_key()));
+    };
+
+    let remove_and_add = move |_| {
+        elements.update(|v| {
+            v.remove(0);
+            v.push(get_next_key());
+        })
+    };
+
+    let shift = move |_| {
+        elements.update(|v| {
+            v.insert(0, get_next_key());
+        })
+    };
+
+    let reset = move |_| {
+        elements.update(|v| {
+            v.clear();
+            next_key.update_value(|v| *v = 6);
+            *v = vec![1, 2, 3, 4, 5];
+        });
+    };
+
+    let remove_two = move |_| {
+        elements.update(|v| {
+            v.pop();
+            v.pop();
+        })
     };
 
     let each = move || elements.get();
@@ -45,15 +78,28 @@ fn HomePage() -> impl IntoView {
 
     let children = move |c: &i32| {
         let c = *c;
+
+        let remove_click = move |_| {
+            elements.update(|v| v.retain(|&x| x != c));
+        };
+
         view! {
-            <div class="element">{c}</div>
+            <button class="element" on:click=remove_click>{c}</button>
         }
     };
 
     view! {
-        <div class="main-grid">
-            <AnimatedFor each key children />
-            <button on:click=on_click>"+ Add"</button>
+        <div class="main-container">
+            <div class="buttons">
+                <button on:click=add_one>"+ Add"</button>
+                <button on:click=remove_and_add>"Remove and Add"</button>
+                <button on:click=shift>"Insert first"</button>
+                <button on:click=remove_two>"Remove 2"</button>
+                <button on:click=reset>"Reset"</button>
+            </div>
+            <div class="main-grid">
+                <AnimatedFor each key children animate_size=true />
+            </div>
         </div>
     }
 }
